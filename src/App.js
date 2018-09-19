02a4c80956e15f2299debb1f7628850030d90639
aguevara1 <albguevara12@gmail.com>
2018-09-19 17:41:31 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,12 +3,30 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state={
+    charity:[]
+  };
+  //https://www.refugerestrooms.org/api/swagger_doc.json
+//  http://img.omdbapi.com/?apikey=[yourkey]&
+//  http://api.open-notify.org/iss-now.json
+// http://www.omdbapi.com/?i=tt3896198&apikey=359b7a74
+componentDidMount(){
+   console.log('here')
+  fetch('https://www.refugerestrooms.org/api/swagger_doc.json')
+  .then(data=>data.json())
+  .then(data=> { console.log(data, 'data')
+  })
+}
+
+
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
+
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
